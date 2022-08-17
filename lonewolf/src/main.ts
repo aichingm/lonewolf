be3f817fa85639b540807ea03151cb228e6a8823
Mario Aichinger <aichingm@gmail.com>
2022-08-17 04:15:19 +0200

    add iconify icons + samples

--- lonewolf/src/main.ts
+++ lonewolf/src/main.ts
@@ -2,10 +2,13 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import naiveUI from 'naive-ui'
+
 // General Font
 import 'vfonts/Lato.css'
 // Monospace Font
 import 'vfonts/FiraCode.css'
+
+import { Icon } from '@iconify/vue';
 
 import App from "./App.vue";
 import router from "./router";
@@ -17,5 +20,7 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(naiveUI);
+
+app.component("Icon", Icon);
 
 app.mount("#app");
--- lonewolf/src/views/AboutView.vue
+++ lonewolf/src/views/AboutView.vue
@@ -12,6 +12,18 @@
       <n-p>
         <n-button>Default</n-button>
       </n-p>
+      <n-p>
+        <n-icon size="40" color="white" :depth="5">
+          <icon icon="fluent:braces-48-regular" />
+        </n-icon>
+      </n-p>
+      <n-p>
+        <n-icon-wrapper :size="40" :border-radius="10">
+          <n-icon size="34" >
+            <icon icon="fluent:checkmark-16-filled" />
+          </n-icon>
+        </n-icon-wrapper>
+      </n-p>
     </div>
   </div>
 </template>
