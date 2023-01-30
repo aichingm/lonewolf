68e8b9e604c0d22f9b4e83560ea8539cdbe4152c
Mario Aichinger <aichingm@gmail.com>
2023-01-30 19:37:56 +0100

    remove BoardView so not to have to maintain it anymore

--- lonewolf/src/router/index.ts
+++ lonewolf/src/router/index.ts
@@ -17,13 +17,6 @@ const router = createRouter({
             // which is lazy-loaded when the route is visited.
             components: {default:() => import("../views/BoardView.vue"), modal:()=>import("../views/ListDialogView.vue")}
         },*/
-        {
-            path: "/board/:board",
-            // route level code-splitting
-            // this generates a separate chunk (BoardView.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import("../views/BoardView.vue")
-        },
     ],
 });
 
